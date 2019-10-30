706676c65c55667737b05a2dca0c5720e27e1104
CQ <caiqing@kedacom.com>
2019-10-30 17:07:19 +0800

    kafka开发

---
+++ boot-kafka/src/main/java/com/caiqing/kafka/bootkafka/controller/KafkaTestController.java
@@ -0,0 +1,34 @@
+package com.caiqing.kafka.bootkafka.controller;
+
+import com.caiqing.kafka.bootkafka.producer.UserLogProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description TODO
+ * @Author CQ
+ * @Date 2019/10/30 14:22
+ **/
+@RestController
+@RequestMapping("/kafka/test")
+public class KafkaTestController {
+
+    @Autowired
+    private UserLogProducer userLogProducer;
+
+    /**
+     * 10.67.8.78:2002/kafka-app/kafka/test/userLog
+     * @param
+     * @return java.lang.String
+     * @author CQ
+     * @date 2019/10/30 14:47
+     */
+    @GetMapping("/userLog")
+    public String kafkaTest(){
+        userLogProducer.sendLog("002");
+        return "发送数据成功";
+    }
+}

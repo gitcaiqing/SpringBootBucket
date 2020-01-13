df52e92f6c1b28f9282986b8ba67558dfd4aa3f3
CQ <caiqing@kedacom.com>
2020-01-13 15:53:19 +0800

    es demo

--- boot-elasticsearch/src/main/java/com/caiqing/bootelasticsearch/controller/EsController.java
+++ boot-elasticsearch/src/main/java/com/caiqing/bootelasticsearch/controller/EsController.java
@@ -3,11 +3,19 @@ package com.caiqing.bootelasticsearch.controller;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.RangeQueryBuilder;
+import org.elasticsearch.search.SearchHit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +23,9 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -129,5 +140,46 @@ public class EsController {
             e.printStackTrace();
             return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    /**
+     * 复合查询   价格小于price的novel
+     * @param author
+     * @param title
+     * @param price
+     * @return org.springframework.http.ResponseEntity
+     * @author CQ
+     * @date 2020/1/13 15:29
+     */
+    @GetMapping("/query/book/novel")
+    public ResponseEntity query(@RequestParam(required = false) String author,
+                                @RequestParam(required = false) String title,
+                                @RequestParam(required = false) Long price){
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        if(!StringUtils.isEmpty(author)){
+            boolQueryBuilder.must(QueryBuilders.matchQuery("author", author));
+        }
+        if(!StringUtils.isEmpty(title)){
+            boolQueryBuilder.must(QueryBuilders.matchQuery("title", title));
+        }
+
+        RangeQueryBuilder rangeQueryBuilder = QueryBuilders.rangeQuery("price").from(0);
+        if(price != null){
+            rangeQueryBuilder.to(price);
+        }
+        boolQueryBuilder.filter(rangeQueryBuilder);
+
+        SearchRequestBuilder searchRequestBuilder = this.transportClient.prepareSearch(ES_INDEX).setTypes(ES_TYPE)
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(boolQueryBuilder)
+                .setFrom(0)
+                .setSize(10);
+        SearchResponse response = searchRequestBuilder.get();
+        List<Map<String, Object>> result = new ArrayList<>();
+        for(SearchHit searchHit : response.getHits()){
+            result.add(searchHit.getSource());
+        }
+        return new ResponseEntity(result, HttpStatus.OK);
+
     }
 }

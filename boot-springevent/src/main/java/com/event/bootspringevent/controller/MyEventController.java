b677c4031e50e4f2faafd4849c90f55a213ad073
caiqing <380120634@qq.com>
2020-05-12 22:49:10 +0800

    event

--- boot-springevent/src/main/java/com/event/bootspringevent/controller/MyEventController.java
+++ boot-springevent/src/main/java/com/event/bootspringevent/controller/MyEventController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
